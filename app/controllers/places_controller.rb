62adce897026da77bbeaa3e929583f6f86b039e2
Saquib Hasnain <saquib.hasnain@kellogg.northwestern.edu>
2024-02-20 11:38:28 +0000

    Final_Phase1_Version

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,32 @@
 class PlacesController < ApplicationController
   def index
-    @places = ["Mexico City", "Charleston", "Beijing", "Amsterdam"]
+    #@places = ["Mexico City", "Charleston", "Beijing", "Amsterdam"]
     # render :template => "places/index"
-    # @places = place.all
+    @places = Place.all
   end
 
   def show
-    @company = companies.find_by
+    @place = Place.find_by({"id" => params["id"]})
+  end
+
+  def new
+    @place = Place.new
+    # render new view with new Place form
+  end
+
+
+  def create
+    # start with a new Place
+    @place = Place.new
+
+    # assign user-entered form data to Place's columns
+    @place["name"] = params["name"]
+
+    # save Place row
+    @place.save
+
+    # redirect user
+    redirect_to "/places"
+  end
+
 end
