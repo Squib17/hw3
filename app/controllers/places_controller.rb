99fac2ce5bcbf255167c1205e2087ac1854eea48
Saquib Hasnain <saquib.hasnain@kellogg.northwestern.edu>
2024-02-20 06:43:55 +0000

    save_3

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,10 @@
 class PlacesController < ApplicationController
   def index
-    
+    @places = ["Mexico City", "Charleston", "Beijing", "Amsterdam"]
+    # render :template => "places/index"
+    # @places = place.all
   end
 
+  def show
+    @company = companies.find_by
 end
